40de2614e6693a9dff37b438814cdc0aca25b5ac
Ly <ly137544897@outlook.com>
2026-07-23 18:52:58 +0800

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/fundkeeper/backend/FundKeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fundkeeper.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundKeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundKeeperBackendApplication.class, args);
+	}
+
+}
